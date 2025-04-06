fe00045647af184978bc052a9bea8d015e2afb2f
Kelly Ku <kellyk1099@gmail.com>
2025-04-06 16:22:50 -0700

    add custom styles and tw themes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,26 +6,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        "light-gray": "#EEEEEE",
+        base: {
+          100: "#ffffff",
+          200: "#e8e8e8",
+          300: "#d1d1d1",
+        },
+        primary: "#66cc89",
+        secondary: "#377bfc",
+        accent: "#f58068",
+        neutral: "#333d4d",
+        info: "#00b5ff",
+        success: "#00aa6e",
+        warning: "#ffbe00",
+        error: "#ff5861",
+        lynkd: "#0466c8",
       },
     },
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: [
-      {
-        linkedin: {
-          primary: "#0A66C2", // LinkedIn Blue
-          secondary: "#FFFFFF", // White
-          accent: "#7FC15E", // LinkedIn Green (for accents)
-          neutral: "#000000", // Black (for text)
-          "base-100": "#F3F2EF", // Light Gray (background)
-          info: "#5E5E5E", // Dark Gray (for secondary text)
-          success: "#057642", // Dark Green (for success messages)
-          warning: "#F5C75D", // Yellow (for warnings)
-          error: "#CC1016", // Red (for errors)
-        },
-      },
-    ],
   },
 };
